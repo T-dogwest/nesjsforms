73ca3125c8aa55f3286926839691f0d56ea33d50
Csontos <csontostibor48@gmail.com>
2023-11-24 09:40:53 +0100

    vegre

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,45 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Registrationdto } from './registration.dto';
+import { error } from 'console';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
   }
+  @Get('register')
+  @Render('registerForm')
+  registerForm() { errors:[] }
+
+  @Post('register')
+  @Render('registerForm')
+  register(@Body() registration: Registrationdto) {
+    const errors: string[] = [];
+
+    if (registration.email.includes('@')) {
+      errors.push('nem jo az email.cim')
+    }
+    if (registration.password.length < 8) {
+      errors.push('a jelszo nem eleg hosszu')
+    }
+    if (registration.passwordagain !== registration.password) {
+      errors.push("nem egyezik a jelszo")
+    }
+    const age = parseInt(registration.age);
+    if (age < 18 || isNaN(age)) {
+      errors.push("nem eleg idos")
+    }
+    if (errors.length > 0) {
+      return { errors, }
+    } else {
+      return {};
+    }
+
+  }
 }
+
---
+++ src/registration.dto.ts
@@ -0,0 +1,6 @@
+export class Registrationdto{
+email:string;
+password:string;
+passwordagain:string;
+age:string;
+} 
